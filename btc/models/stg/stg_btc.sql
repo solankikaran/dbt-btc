324d532530d67de8e602ec13df7d3e89892a86fe
Karan Solanki <solankikaran05@gmail.com>
2026-06-06 19:09:21 +0530

    converted stg into incremental

--- btc/models/stg/stg_btc.sql
+++ btc/models/stg/stg_btc.sql
@@ -1,2 +1,19 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='merge',
+        unique_key='hashkey'
+    )
+}}
+
 SELECT *
 FROM {{ source('raw', 'btc') }}
+
+{% if is_incremental() %}
+
+WHERE block_timestamp >= (
+    SELECT MAX(block_timestampe) 
+    FROM {{ this }}
+)
+
+{% endif %}
