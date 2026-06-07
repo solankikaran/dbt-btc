b538946fbef859f3d4ce64d19cdf694a7a0dda9b
Karan Solanki <solankikaran05@gmail.com>
2026-06-07 11:25:45 +0530

    added a custom generic test

---
+++ btc/tests/generic/crypto_utils.sql
@@ -0,0 +1,7 @@
+{% test assert_valid_btc_address(model, column_name) %}
+
+SELECT *
+FROM {{ model }}
+WHERE NOT (({{ column_name }} LIKE '1%') OR ({{ column_name }} LIKE '3%') OR ({{ column_name }} LIKE 'bc1%') OR ({{ column_name }} LIKE 'bc1p%'))
+
+{% endtest %}
